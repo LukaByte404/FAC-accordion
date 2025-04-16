4b57c28179f8151261e672d9c52183a1eed03a4f
ChikhladzeLuka <lukachikhladze587@gmail.com>
2025-04-16 16:17:01 +0400

    i make many design issue but i add javascript that changes icons from plus to minus and from minus to plus

--- faq-accordion-main/mian.js
+++ faq-accordion-main/mian.js
@@ -1,12 +1,15 @@
 // 2. მე მინდა რომ, როდესაც დავაჭერ icon-ს კითხვის ქვემოთ გამოჩნდეს ტექსი
 
 const plusBtn = document.querySelectorAll(".plus-btn");
-const plusImg = document.querySelector(".plus-img");
 
-function iconChanger() {
-  if (plusImg.src.includes("icon-minus.svg")) {
-    plusImg.src = "assets/images/icon-plus.svg";
-  } else if (plusImg.src.includes("icon-plus.svg")) {
-    plusImg.src = "assets/images/icon-minus.svg";
-  }
-}
+plusBtn.forEach((button) => {
+  button.addEventListener("click", () => {
+    const img = button.querySelector(".plus-img");
+
+    if (img.src.includes("icon-minus.svg")) {
+      img.src = "assets/images/icon-plus.svg";
+    } else if (img.src.includes("icon-plus.svg")) {
+      img.src = "assets/images/icon-minus.svg";
+    }
+  });
+});
