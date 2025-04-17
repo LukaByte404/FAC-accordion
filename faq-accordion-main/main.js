66934d2b347cdfd8d0f5b2db45104b1091def551
ChikhladzeLuka <lukachikhladze587@gmail.com>
2025-04-17 13:02:30 +0400

    add javascript that makes toggle and fix design

--- faq-accordion-main/main.js
+++ faq-accordion-main/main.js
@@ -1,15 +1,19 @@
 // 2. მე მინდა რომ, როდესაც დავაჭერ icon-ს კითხვის ქვემოთ გამოჩნდეს ტექსი
 
 const plusBtn = document.querySelectorAll(".plus-btn");
+const text = document.querySelectorAll(".answer");
 
 plusBtn.forEach((button) => {
   button.addEventListener("click", () => {
     const img = button.querySelector(".plus-img");
+    const answer = button.closest(".faq-item").querySelector("p");
 
     if (img.src.includes("icon-minus.svg")) {
       img.src = "assets/images/icon-plus.svg";
+      answer.classList.add("answer-show");
     } else if (img.src.includes("icon-plus.svg")) {
       img.src = "assets/images/icon-minus.svg";
+      answer.classList.remove("answer-show");
     }
   });
 });
